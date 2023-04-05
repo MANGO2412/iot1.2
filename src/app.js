88ed30561d1b3d18490feeee71c1a8b17dcda54f
MANGO2412 <manuelgom3z22052003@gmail.com>
2023-04-04 20:03:06 -0700

    version 0.1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+
 
 
 //public static 
@@ -11,6 +11,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports=app;
